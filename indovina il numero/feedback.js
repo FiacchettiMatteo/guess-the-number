842dbc4212430ee648ce7e4866f6819c27e57d96
FiacchettiMatteo <150785951+FiacchettiMatteo@users.noreply.github.com>
2023-12-22 17:42:12 +0100

    Delete indovina il numero directory

--- indovina il numero/feedback.js
+++
@@ -1,9 +0,0 @@
-let outName = document.getElementById('box-name')
-let outEmail = document.getElementById('box-email')
-let outFeedback = document.getElementById('box-feedback')
-
-let urlParam = new URLSearchParams(window.location.search)
-
-outName.innerHTML = urlParam.get('fname')
-outEmail.innerHTML = urlParam.get('email').toLowerCase()
-outFeedback.innerHTML = urlParam.get('feedback')
